e3bedf23f04e70831f6c6a89ae826c24a32ffcd9
giovannagabrielysouza-max <giovanna.gabriely.souza@escola.pr.gov.br>
2026-05-11 12:16:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -38,6 +38,24 @@ segundos +
 " segundos"
 );
 }
+function calculaTempo(tempoObjetivo) {
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+return (
+dias +
+" dias " +
+horas +
+" horas " +
+minutos +
+" minutos " +
+segundos +
+" segundos"
+);
+}
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
